47f91ca33099f8181fec59d79910c359e0bab840
willycole12345 <cole.william043@gmail.com>
2022-10-17 12:47:39 +0100

    package updated

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,6 @@ import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AccountsRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new AccountsRoute(), new AuthRoute()]);
 
 app.listen();
