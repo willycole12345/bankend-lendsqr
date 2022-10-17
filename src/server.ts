72b320caeb77b970de17d801b2d833e439137156
willycole12345 <cole.william043@gmail.com>
2022-10-17 13:00:32 +0100

    new route added

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import App from '@/app';
 import AuthRoute from '@routes/auth.route';
 import IndexRoute from '@routes/index.route';
 import AccountsRoute from '@routes/accounts.route';
+import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new AccountsRoute(), new AuthRoute()]);
+const app = new App([new IndexRoute(), new UsersRoute(), new AccountsRoute(), new AuthRoute()]);
 
 app.listen();
